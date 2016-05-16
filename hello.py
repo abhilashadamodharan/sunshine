a3a145fc95eee97e274503af4bd10b1b226b7912
APPLE <apple@APPLEs-MacBook-Pro.local>
2016-05-16 18:45:40 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python -tt
+
+import sys
+
+def main():
+     if len(sys.argv) >= 2:
+        name = sys.argv[1]
+        a = len(sys.argv[1])
+     else:
+         name = 'world!'
+         a = 0
+#     print 'hello'   
+     print 'hello', name, a
+    
+if __name__ == '__main__':
+    main()
+
